12fad3bfb9a229e1faa92f17e11498b09eb8bf5f
liangzixuan007 <104885831+liangzixuan007@users.noreply.github.com>
2022-05-04 14:00:57 +0800

    Add files via upload

---
+++ 盾牌.py
@@ -0,0 +1,57 @@
+import turtle as t
+t.speed(10)
+
+t.penup()
+t.goto(0,-200)
+t.pendown()
+t.color('red')
+
+t.begin_fill()
+t.circle(200)
+t.end_fill()
+
+t.penup()
+t.goto(0,-150)
+t.pendown()
+t.color('white')
+
+t.begin_fill()
+t.circle(150)
+t.end_fill()
+
+t.penup()
+t.goto(0,-100)
+t.pendown()
+t.color('red')
+
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+
+
+t.penup()
+t.goto(0,-50)
+t.pendown()
+t.color('blue')
+
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+t.penup
+t.goto(-40,10)
+t.pendown()
+
+t.color('white')
+
+t.begin_fill()
+
+for i in range(5):
+    t.forward(80)
+    t.right(144)
+
+t.end_fill()
+
+t.hideturtle()
+
+    
